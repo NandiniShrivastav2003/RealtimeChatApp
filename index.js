1f200b4f8e6903a7236d47d260610d08827ec7f6
nandini <nandinishri2208@gmail.com>
2022-07-16 19:12:55 +0530

    receiving and sending some messages from client and server side

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
+const http=require('http');
 const express=require('express');
 const app=express();
+const server=http.createServer(app);
+//setting upsocket
+const socketio=require('socket.io');
+const io=socketio(server)
 
-
-
+io.on("connection",(socket)=>{
+    console.log('connected with socket id=',socket.id);
+    socket.on('boom',() =>{
+        console.log('boom received from',socket.id);
+    })
+    setInterval(() =>{
+        socket.emit('whizz')
+    },2000)
+})
 //include public folder in app
 //1st way 
 app.use('/', express.static(__dirname  +'/public'));
+
 //2nd way
 // app.use(express.static('public'));
 // app.get('/',function(req,res) {
 //     res.sendFile('index.html');
 //   });
-app.listen(2000,()=>{
+server.listen(2000,()=>{
     console.log('server is running on:http://localhost:2000')
 })
